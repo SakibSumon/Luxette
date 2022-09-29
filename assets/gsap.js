56c25b0651cc27e33f0203f5c365703bb10ca457
SakibSumon <sakib.cse.27@gmail.com>
2022-09-30 00:22:16 +0600

    hero parallax done

--- assets/gsap.js
+++ assets/gsap.js
@@ -11,5 +11,35 @@ var flkty = new Flickity('.main-carousel', {
     adaptiveHeight: true,
     pageDots: true,
     accessibility: false,
-    wrapAround: true
+    wrapAround: true,
+   
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to('.carousel-cell', {
+//     scrollTrigger: {
+//         trigger: '.carousel-cell',
+//         start: 'top 0',
+       
+//         scrub: 1,
+//     },
+//     y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+//   ease: "none"    
+// })
+
+
+// apply parallax effect to any element with a data-speed attribute
+gsap.utils.toArray("[data-speed]").forEach(el => {
+  gsap.to(el, {
+    y: function() {return (1 - parseFloat(el.getAttribute("data-speed"))) * (ScrollTrigger.maxScroll(window) - (this.scrollTrigger ? this.scrollTrigger.start : 0))},
+    ease: "none",
+    scrollTrigger: {
+      trigger: el,
+      start: "top 0%",
+      end: "max",
+      invalidateOnRefresh: true,
+      scrub: true
+    }
+  });
 });
